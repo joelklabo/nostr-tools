67f023b39d442b9aecaaccc42eba3350167fcb20
Joel Klabo <joelklabo@gmail.com>
2023-04-19 21:47:50 -0700

    Try Adding crypto to globalThis

--- index.ts
+++ index.ts
@@ -23,6 +23,8 @@ export * as utils from './utils'
 import * as secp256k1 from '@noble/secp256k1'
 import {hmac} from '@noble/hashes/hmac'
 import {sha256} from '@noble/hashes/sha256'
+import crypto from 'crypto'
+globalThis.crypto = crypto
 secp256k1.utils.hmacSha256Sync = (key, ...msgs) =>
   hmac(sha256, key, secp256k1.utils.concatBytes(...msgs))
 secp256k1.utils.sha256Sync = (...msgs) =>
